ce1aa264652d907f4c45bcbdf116e48930ef29ef
Danne <danne_7875@hotmail.com>
2017-08-27 21:10:09 +0200

    fixat med outlaw flyttat blackmarket samt ändrat i weapon shop på bl

--- Config_Weapons.hpp
+++ Config_Weapons.hpp
@@ -25,12 +25,11 @@ class WeaponShops {
             { "hgun_Rook40_F", "", 6500, 500 },
             { "hgun_Pistol_heavy_02_F", "", 9850, -1 },
             { "hgun_ACPC2_F", "", 11500, -1 },
-            { "hgun_PDW2000_F", "", 45000, -1 },
             { "hgun_Pistol_01_F", "", 16000, -1 }
         };
         mags[] = {
             { "16Rnd_9x21_Mag", "", 25 },
-			 { "10Rnd_9x21_Mag", "", 25 },
+			{ "10Rnd_9x21_Mag", "", 25 },
             { "6Rnd_45ACP_Cylinder", "", 50 },
             { "9Rnd_45ACP_Mag", "", 45 },
             { "30Rnd_9x21_Mag", "", 75 },
@@ -381,28 +380,37 @@ class WeaponShops {
         license = "";
         level[] = { "", "", -1, "" };
         items[] = {
-            { "hgun_Rook40_F", "", 10000, 500 },
-            { "hgun_Pistol_heavy_02_F", "", 10500, -1 },
-            { "hgun_ACPC2_F", "", 11500, -1 },
-            { "hgun_PDW2000_F", "", 60000, -1 },
-            { "srifle_DMR_01_F","", 800000, -1 }, // Rahim				
-			{ "arifle_Mk20_F", "", 410000, 500 },
-			{ "arifle_CTAR_blk_F", "", 320000, 500 },
-			{ "arifle_SDAR_F", "",   50000, 15000 -1 }, // SDAR
-			{ "arifle_AKS_F", "", 185000, 500 }
+            // 9 mm
+            { "SMG_01_F",               "", 15000, -1 }, // SMG
+            { "hgun_Rook40_F",          "", 6500,   -1 }, // ROOK
+            { "hgun_PDW2000_F",         "", 12000, -1 }, // PDW
+			{ "hgun_P07_F",         	 "", 10000,   -1 }, // p07
+			{ "SMG_05_F",         	     "", 10000,   -1 }, // protector
+		    { "arifle_SDAR_F",          "",   50000, 15000 -1 }, // SDAR
+
+            // .45 ACP
+            { "hgun_ACPC2_F",           "", 7500,   -1 }, // .45 ACP
+            { "hgun_Pistol_heavy_01_F", "", 7500,   -1 },  // .45 ACP
+			{ "hgun_Pistol_heavy_02_F", "", 7500,   -1 }  // .45 Revolver
+			{ "arifle_SDAR_F", "",   50000, 15000 -1 } // SDAR
 		
         };
         mags[] = {
-            { "16Rnd_9x21_Mag", "", 25 },
-            { "6Rnd_45ACP_Cylinder", "", 50 },
-            { "9Rnd_45ACP_Mag", "", 45 },
-            { "30Rnd_556x45_Stanag", "", 1000 },
-			{ "20Rnd_556x45_UW_mag",    "", 1000,   -1 }, // 5.56 Underwater Stanag
-			{ "30Rnd_545x39_Mag_F", "", 1000 },
-            { "9Rnd_45ACP_Mag", "", 45 },
-			{ "30Rnd_580x42_Mag_F",     "", 1000,   -1 }, // CTAR
-			{ "10Rnd_762x54_Mag",    "", 1000,   -1 }, // Rahim	
-            { "30Rnd_9x21_Mag", "", 75 }
+            // 5.56
+		    { "30Rnd_556x45_Stanag",    "", 1000,   -1 }, // 5.56 Stanag
+            { "20Rnd_556x45_UW_mag",    "", 1000,   -1 }, // 5.56 Underwater Stanag
+			
+            // .45 ACP
+            { "30Rnd_45ACP_Mag_SMG_01", "", 1000,   -1 }, // .45 ACP
+            { "9Rnd_45ACP_Mag",         "", 900,    -1 }, // .45 ACP Handgun mag
+			{ "11Rnd_45ACP_Mag",         "", 900,    -1 }, // .45 ACP Handgun mag
+			{ "6Rnd_45ACP_Cylinder",         "", 900,    -1 }, // .45 ACP Handgun mag
+			
+
+            // 9 mm
+            { "16Rnd_9x21_Mag",         "", 600,    -1 }, // 9mm 16rnd
+            { "30Rnd_9x21_Mag",         "", 800,    -1 }, // 9mm 30rnd
+			{ "30Rnd_9x21_Mag_SMG_02",         "", 800,    -1 } // 9mm 30rnd
         };
         accs[] = {
             { "optic_ACO_grn_smg", "", 1950 },
